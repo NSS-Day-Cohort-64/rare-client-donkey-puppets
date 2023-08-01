09608babe7b5a4757ddb2789d5a46727025ca1ac
Logan Belews <Loganbelew2@gmail.com>
2023-08-01 13:57:51 -0500

    all posts view

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 
 export const Post = ({ post }) => (
    <li>
-    {post.title}
+    <h1></h1>
+    <div>{post.title}</div>
+    <div>{post.user.first_name} {post.user.last_name}</div>
+    <div> {post.category.label}</div>
+    <div>{post.publication_date}</div>
    </li>
   )
