9c674e056f714fc44b33aeec935d0b9e8a4a80e8
Logan Belews <Loganbelew2@gmail.com>
2023-08-01 10:33:33 -0500

    added partial posts features

---
+++ src/components/posts/Post.js
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+export const Post = ({ post }) => (
+   <li>
+    {post.title}
+   </li>
+  )
---
+++ src/managers/PostManager.js
@@ -0,0 +1,5 @@
+export const getAllPosts = () => {
+    return fetch(`http://localhost:8088/Posts`)
+    .then( res => res.json())
+}
+    
