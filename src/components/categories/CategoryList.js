dfb50c0a5d5f1db9d3992323057e963dedd9f789
Logan Welch <loganewelch@gmail.com>
2023-08-02 14:08:27 -0500

    commit

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Category from "./Category";
+import { Category } from "./Category";
 
 const CategoryList = () => {
   const [categories, setCategories] = useState([]);
