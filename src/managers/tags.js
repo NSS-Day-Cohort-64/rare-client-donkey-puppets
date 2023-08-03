37f002afa1503c2cdc656aa7545e88382d48f75b
Logan Welch <loganewelch@gmail.com>
2023-08-03 11:32:09 -0500

    resolved yet another conflict

--- src/managers/tags.js
+++ src/managers/tags.js
@@ -1,7 +1,6 @@
 export const getTags = () => {
-<<<<<<< HEAD
-    return fetch("http://localhost:8088/tags")
-        .then(res => res.json())
+  return fetch("http://localhost:8088/tags")
+      .then(res => res.json())
 }
 export const createTag = (newTag) => {
     return fetch("http://localhost:8088/tags", {
@@ -12,19 +11,5 @@ export const createTag = (newTag) => {
         body: JSON.stringify(newTag),
     })
         .then((response) => response.json());
-=======
-  return fetch("http://localhost:8088/tags")
-      .then(res => res.json())
-}
-
-export const createTag = (newTag) => {
-  return fetch("http://localhost:8088/tags", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newTag),
-  })
-  .then((response) => response.json());
->>>>>>> main
 };
+
