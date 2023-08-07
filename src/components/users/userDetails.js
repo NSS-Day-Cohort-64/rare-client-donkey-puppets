a88a3f52d140271237e1e04eb8a74b94067b2a87
Randy Hamm <randykhamm@gmail.com>
2023-08-07 09:20:37 -0500

    merged with new main

--- src/components/users/userDetails.js
+++ src/components/users/userDetails.js
@@ -1,24 +1,63 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
+import {  createSubscription } from "../../managers/SubscriptionManager";
+
 
 export const UserDetails = () => {
     const [user, setUser] = useState({})
     const { userId } = useParams()
+    const navigate = useNavigate()
+    const rareUserId = localStorage.getItem("auth_token");
+    const rareUser = JSON.parse(rareUserId);
+
     useEffect(() => {
         fetch(`http://localhost:8088/users/${userId}`)
         .then(res => res.json())
         .then(data => setUser(data))
     },[])
 
-    return <>
-    {user.first_name} {user.last_name}
-    <br/>
-    {user.profile_image_url}
-    <br/>
-    {user.username}
-    <br/>
-    {user.created_on}
-    <br/>
-    {user.bio}
-    </>
+    
+    const createNewSubscription = () => {
+        return {
+            follower_id: rareUser,
+            author_id: parseInt(userId),
+            created_on: getCurrentDate()
+        };
+    };
+
+    const getCurrentDate = () => {
+        const currentDate = new Date()
+        const formattedDate = currentDate.toDateString().substring(4)
+        return formattedDate
+    }
+    
+    const handleSubscribe = () => {
+        const newSubscription = createNewSubscription();
+    
+        createSubscription(newSubscription)
+            .then(response => {
+                console.log("Subscription added:", response);
+                navigate("/");
+            })
+            .catch(error => {
+                console.error("Error subscribing:", error);
+            });
+    };
+    
+
+    return (
+        <>
+            {user.first_name} {user.last_name}
+            <br/>
+            {user.profile_image_url}
+            <br/>
+            {user.username}
+            <br/>
+            {user.created_on}
+            <br/>
+            {user.bio}
+            <br/>
+            <button onClick={handleSubscribe} className="button is-primary">Subscribe</button>
+        </>
+    )
 }
