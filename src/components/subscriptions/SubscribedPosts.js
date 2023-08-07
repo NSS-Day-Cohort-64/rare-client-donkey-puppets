3c943aa09ec2a15721d5751fdcecfa76b5ba5b87
Logan Belews <Loganbelew2@gmail.com>
2023-08-07 11:57:13 -0500

    added subscribed posts component on home page view

--- src/components/subscriptions/SubscribedPosts.js
+++ src/components/subscriptions/SubscribedPosts.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react"
 import { GetSubscribedPosts } from "../../managers/SubscriptionManager"
+import { Post } from "../posts/Post"
+import { Link } from "react-router-dom"
+import "./subscribed.css"
 
 export const SubscribedPosts = () => {
     const [subscribedPosts, setSubscribedPosts] = useState([])
@@ -8,5 +11,25 @@ export const SubscribedPosts = () => {
         .then(data => setSubscribedPosts(data))
     },[])
 
-    
+    return <>
+    <h2>Your Subscriptions</h2>
+    {subscribedPosts.map(p =>
+    <div key={p.id}>
+        <ul className="subscribedPostsList">
+            <li className="list-item">
+            view post - <Link to={`/postDetails/${p.post.id}`}>{p.post.title}</Link>  
+            </li>
+            <li className="list-item">
+            {p.post.content}
+            </li>
+            <li className="list-item">
+            publication date {p.post.publication_date}
+            </li>
+            <li className="list-item">
+            category: {p.category.label}
+            </li>
+        </ul>
+    </div> 
+    )}
+    </>
 }
